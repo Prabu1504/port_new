10a47f0b8a4c261a4186421d4780df0c27ab0f7a
Prabu-wgs <prabu@whyglobalservices.com>
2024-10-26 14:29:42 +0530

    list

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import logo from "../assets/logo.png"
-import { Button, Input, message, Modal, Spin } from 'antd';
+import {Input, Modal, Spin } from 'antd';
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -66,7 +66,7 @@ const Navbar = () => {
                 />
 
                 {/* Portfolio Text */}
-                <h2 className="pacifico-regular text-white sm:text-2xl md:text-3xl lg:text-4xl ml-4 sm:ml-6 md:ml-8 lg:ml-10 p-2 hover:text-[#00df9a]">
+                <h2 className="pacifico-regular inline-block transform transition duration-300 ease-out hover:scale-125 hover:text-blue-600 text-white sm:text-2xl md:text-3xl lg:text-4xl ml-4 sm:ml-6 md:ml-8 lg:ml-10 p-2">
                     Portfolio
                 </h2>
             </div>
@@ -75,27 +75,26 @@ const Navbar = () => {
                 <ul>
                     <li><Link to="/ContactUs">
                         <button onClick={showModal}
-                            className='bg-white py-1 px-3 text-black hover:text-orange-600 transition duration-200 ease-in-out rounded-lg text-sm md:text-base lg:text-lg xl:text-xl'>
+                            className='bg-white py-1.5 px-3  text-black hover:bg-blue-600 hover:text-white  transition duration-200 ease-in-out rounded-lg text-sm md:text-base lg:text-lg xl:text-xl'>
                             Contact Us</button>
                     </Link></li>
                     <Modal
-
                         open={isModalOpen}
                         onOk={handleOk}
                         onCancel={handleCancel}>
                         <div className='user-form'>
                             <div className='heading'>
                                 {isLoading && <Spin />}{error && <p>Error:{error}</p>}
-                                <h2 className='text-xl font-semibold justify-center flex items-center underline underline-offset-3'>Context Me</h2>
+                                <h2 className='text-xl font-semibold justify-center flex text-blue-600 items-center underline underline-offset-4'>Context Me</h2>
 
                             </div>
                         </div>
                         <form onSubmit={handleSubmit}>
                             <div className=' flex flex-col gap-4'>
-                                <label >Name: <Input type='text' id='name' value={user.name} name='name' onChange={handleInput} className='border-2 rounded px-3' placeholder='Name' ></Input></label>
-                                <label>Email: <Input type='email' id='email' value={user.email} name='email' onChange={handleInput} className='border-2 border-gray-600 rounded px-3' placeholder='Email'></Input></label>
-                                <label >Mobile No: <Input type='number' id="mobile" value={user.mobile} name="mobile" onChange={handleInput} className='border-2 border-gray-600 rounded px-2' placeholder='Phone Number'></Input></label>
-                                <label >Message: <Input type='textarea' id='message' value={user.message} name="message" onChange={handleInput} className='border-2 border-gray-600 rounded px-3' placeholder='Message'></Input></label>
+                                <label >Name: <Input type='text' id='name' value={user.name} name='name' onChange={handleInput} className=' rounded px-3' placeholder='Name' ></Input></label>
+                                <label>Email: <Input type='email' id='email' value={user.email} name='email' onChange={handleInput} className='rounded px-3' placeholder='Email'></Input></label>
+                                <label >Mobile No: <Input type='number' id="mobile" value={user.mobile} name="mobile" onChange={handleInput} className=' rounded px-2' placeholder='Phone Number'></Input></label>
+                                <label >Message: <Input type='textarea' id='message' value={user.message} name="message" onChange={handleInput} className=' rounded px-3' placeholder='Message'></Input></label>
                             </div>
                         </form>
                     </Modal>
